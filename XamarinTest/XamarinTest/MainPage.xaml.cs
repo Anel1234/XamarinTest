9ac2bdabdf85f7b156d76908e93c942b205adb44
GIDEV\apilav <anel.pilav@gmail.com>
2019-06-20 16:46:02 +0100

    initial commit

---
+++ XamarinTest/XamarinTest/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XamarinTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void GetLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.High);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                Console.WriteLine("Handle not supported");
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                Console.WriteLine("Handle not enabled");
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                Console.WriteLine("Handle not permitted");
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to get location");
+                // Unable to get location
+            }
+        }
+    }
+}
